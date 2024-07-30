86bb9985dc3ce641042e6d12d3d53d5b6a13e88a
mahat07 <140910546+mahat07@users.noreply.github.com>
2024-07-30 22:06:47 +0530

    Create Design patterns and principles Task 09.java

---
+++ Design patterns and principles Task 09.java
@@ -0,0 +1,77 @@
+public interface Command {
+    void execute();
+}
+public class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOn();
+    }
+}
+public class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOff();
+    }
+}
+public class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        if (command != null) {
+            command.execute();
+        } else {
+            System.out.println("No command assigned.");
+        }
+    }
+}
+public class Light {
+    private String location;
+
+    public Light(String location) {
+        this.location = location;
+    }
+
+    public void turnOn() {
+        System.out.println(location + " light is on.");
+    }
+
+    public void turnOff() {
+        System.out.println(location + " light is off.");
+    }
+}
+public class CommandPatternExample {
+
+    public static void main(String[] args) {
+        // Receiver
+        Light livingRoomLight = new Light("Living Room");
+
+        // Concrete Commands
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+        // Invoker
+        RemoteControl remote = new RemoteControl();
+
+        // Turn the light on
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        // Turn the light off
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
