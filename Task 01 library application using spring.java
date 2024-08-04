1c66193db666a5772614812a9376c327c7af9acf
mahat07 <140910546+mahat07@users.noreply.github.com>
2024-08-04 13:12:40 +0530

    Create Task 01 library application using spring.java

---
+++ Task 01 library application using spring.java
@@ -0,0 +1,40 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void performService() {
+        bookRepository.getBooks();
+    }
+}
+
+package com.library.repository;
+
+public class BookRepository {
+    public void getBooks() {
+        System.out.println("Fetching books from the repository.");
+    }
+}
+
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = (BookService) context.getBean("bookService");
+        bookService.performService();
+    }
+}
+
+
